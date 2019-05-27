d02ea0ccf11c78115c945fbd26e58230b4489279
GeorgeChrst <32521291+GeorgeChrst@users.noreply.github.com>
2019-05-27 20:22:44 +0300

    xfdhgbs
    sdfs

--- Code/Prepared Data/DataPrep.py
+++ Code/Prepared Data/DataPrep.py
@@ -1,5 +1,4 @@
 import gzip
-import simplejson
 import re
 from nltk.corpus import stopwords
 import pandas as pd
@@ -161,6 +160,62 @@ def count_words(count_dict, text):
             else:
                 count_dict[word] += 1
 
+def write(file, text):
+    with open(file, 'wb') as fp:
+        pickle.dump(text, fp)
+
+def read(file):
+    with open(file, 'rb') as fp:
+        return pickle.load(fp)
+
+def missing_words(word_counts,embeddings_index):
+    missing_words = 0
+    for word, count in word_counts.items():
+        if count > threshold:
+            if word not in embeddings_index:
+                missing_words += 1
+
+    missing_ratio = round(missing_words / len(word_counts), 4) * 100
+
+    print("Number of words missing from CN:", missing_words)
+    print("Percent of words that are missing from vocabulary: {}%".format(missing_ratio))
+
+def convert_to_ints(text, word_count, unk_count, eos=False):
+    '''Convert words in text to an integer.
+       If word is not in vocab_to_int, use UNK's integer.
+       Total the number of words and UNKs.
+       Add EOS token to the end of texts'''
+    ints = []
+    for sentence in text:
+        sentence_ints = []
+        for word in sentence.split():
+            word_count += 1
+            if word in vocab_to_int:
+                sentence_ints.append(vocab_to_int[word])
+            else:
+                sentence_ints.append(vocab_to_int["<UNK>"])
+                unk_count += 1
+        if eos:
+            sentence_ints.append(vocab_to_int["<EOS>"])
+        ints.append(sentence_ints)
+    return ints, word_count, unk_count
+
+def create_lengths(text):
+    '''Create a data frame of the sentence lengths from a text'''
+    lengths = []
+    for sentence in text:
+        lengths.append(len(sentence))
+    return pd.DataFrame(lengths, columns=['counts'])
+
+def unk_counter(sentence):
+    '''Counts the number of time UNK appears in a sentence.'''
+    unk_count = 0
+    for word in sentence:
+        if word == vocab_to_int["<UNK>"]:
+            unk_count += 1
+    return unk_count
+
+
 
 
 itemCounter = 0
@@ -172,35 +227,45 @@ sentences = []
 labels = []
 threshold=2
 
-for e in parse(file):
-    ent = simplejson.dumps(e)
+# for e in parse(file):
+#     ent = simplejson.dumps(e)
+#
+#     l_start = ent.find(label_start) + len(label_start)
+#     l_end = ent.find(review_start) - 3
+#     label = ent[l_start:l_end]  # extract label
+#
+#     s_start = ent.find(review_start)+len(review_start)
+#     s_end = ent.find("\"}")
+#     sentence = ent[s_start:s_end]  # extract review
+#
+#     if s_start != -1 and l_start != -1:
+#
+#         sentence, label = clean_text(sentence, label, remove_stopwords=True)
+#         # sentence einai olo to review ka8arismeno
+#         # label einai to summary ka8arismeno
+#         sentences.append(sentence)
+#         labels.append(label)
+#         itemCounter += 1
+#
+# word_dict={}
+#
+#
+# write('Cleaned_text',sentences)
+# write('Cleaned_labels', labels)
+#
+# count_words(word_dict,sentences)
+# count_words(word_dict, labels)
+#
+# print("Size of Vocabulary:", len(word_dict))
+# write("Vocabulary",word_dict)
+#
 
-    l_start = ent.find(label_start) + len(label_start)
-    l_end = ent.find(review_start) - 3
-    label = ent[l_start:l_end]  # extract label
 
-    s_start = ent.find(review_start)+len(review_start)
-    s_end = ent.find("\"}")
-    sentence = ent[s_start:s_end]  # extract review
+word_dict = {}
+word_dict = read("Vocabulary")
 
-    if s_start != -1 and l_start != -1:
-
-        sentence, label = clean_text(sentence, label, remove_stopwords=True)
-        # sentence einai olo to review ka8arismeno
-        # label einai to summary ka8arismeno
-        sentences.append(sentence)
-        labels.append(label)
-        itemCounter += 1
-
-word_dict={}
-
-count_words(word_dict,sentences)
-count_words(word_dict, labels)
-
-print("Size of Vocabulary:", len(word_dict))
-with open('Vocabulary','wb') as fp:
-    pickle.dump(word_dict,fp)
-
+sentences=read('Cleaned_text')
+labels=read('Cleaned_labels')
 
 # embeddings_index = {}
 # with open('numberbatch.txt', encoding='utf-8') as f:
@@ -216,8 +281,12 @@ with open('Vocabulary','wb') as fp:
 # with open('embIndex_pickle','wb') as fp:
 #     pickle.dump(embeddings_index,fp)
 
-with open('embIndex_pickle','rb') as fp:
-    embeddings_index=pickle.load(fp)
+# with open('embIndex_pickle','rb') as fp:
+#     embeddings_index=pickle.load(fp)
+
+#write('embIndex_pickle',embeddings_index)
+
+embeddings_index = read('embIndex_pickle')
 
 print(len(embeddings_index))
 
@@ -250,19 +319,67 @@ print("Percent of words we will use: {}%".format(usage_ratio))
 
 embedding_dim = 300
 nb_words = len(vocab_to_int)
-
-# Create matrix with default values of zero
-word_embedding_matrix = np.zeros((nb_words, embedding_dim), dtype=np.float32)
-for word, i in vocab_to_int.items():
-    if word in embeddings_index:
-        word_embedding_matrix[i] = embeddings_index[word]
-    else:
-        # If word not in CN, create a random embedding for it
-        new_embedding = np.array(np.random.uniform(-1.0, 1.0, embedding_dim))
-        embeddings_index[word] = new_embedding
-        word_embedding_matrix[i] = new_embedding
-
-# Check if value matches len(vocab_to_int)
-print(len(word_embedding_matrix))
-with open('emb_matrix','wb') as fp:
-    pickle.dump(word_embedding_matrix,fp)
+write('vocab_to_int', vocab_to_int )
+#
+# # Create matrix with default values of zero
+# # word_embedding_matrix = np.zeros((nb_words, embedding_dim), dtype=np.float32)
+# # for word, i in vocab_to_int.items():
+# #     if word in embeddings_index:
+# #         word_embedding_matrix[i] = embeddings_index[word]
+# #     else:
+# #         # If word not in CN, create a random embedding for it
+# #         new_embedding = np.array(np.random.uniform(-1.0, 1.0, embedding_dim))
+# #         embeddings_index[word] = new_embedding
+# #         word_embedding_matrix[i] = new_embedding
+#
+# # Check if value matches len(vocab_to_int)
+# # print(len(word_embedding_matrix))
+# # with open('emb_matrix','wb') as fp:
+# #     pickle.dump(word_embedding_matrix,fp)
+#
+# #write('emb_matrix',word_embedding_matrix)
+#
+# word_embedding_matrix = read('emb_matrix')
+# print(len(word_embedding_matrix))
+#
+# word_count = 0
+# unk_count = 0
+#
+# int_labels, word_count, unk_count = convert_to_ints(labels, word_count, unk_count)
+# int_sentences, word_count, unk_count = convert_to_ints(sentences, word_count, unk_count, eos=True)
+#
+# unk_percent = round(unk_count/word_count,4)*100
+#
+# print("Total number of words in headlines:", word_count)
+# print("Total number of UNKs in headlines:", unk_count)
+# print("Percent of words that are UNK: {}%".format(unk_percent))
+#
+# lengths_summaries = create_lengths(int_labels)
+# lengths_texts = create_lengths(int_sentences)
+#
+# sorted_summaries = []
+# sorted_texts = []
+# max_text_length = 84
+# max_summary_length = 13
+# min_length = 2
+# unk_text_limit = 1
+# unk_summary_limit = 0
+#
+# for length in range(min(lengths_texts.counts), max_text_length):
+#     for count, words in enumerate(int_labels):
+#         if (len(int_labels[count]) >= min_length and
+#                 len(int_labels[count]) <= max_summary_length and
+#                 len(int_sentences[count]) >= min_length and
+#                 unk_counter(int_labels[count]) <= unk_summary_limit and
+#                 unk_counter(int_sentences[count]) <= unk_text_limit and
+#                 length == len(int_sentences[count])
+#         ):
+#             sorted_summaries.append(int_labels[count])
+#             sorted_texts.append(int_sentences[count])
+#
+# # Compare lengths to ensure they match
+# print(len(sorted_summaries))
+# print(len(sorted_texts))
+#
+# write('Sorted_labels',sorted_summaries)
+# write('Sorted_data',sorted_texts)
